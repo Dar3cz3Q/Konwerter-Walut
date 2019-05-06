f08329118f3de1ed6dc5ef8eae36672f694745ce
Dar3cz3Q <dho1@tlen.pl>
2019-05-06 19:50:22 +0200

    Maked a converter

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -27,13 +27,97 @@ function TableDownloaded(data, TableCode) {
 }
 
 function TableSuccess(data) {
-    console.log(data);
+    $('.ValueSelect').html('');
+    $(".MoneyList").html('');
+
+    $('.ValueSelect').html(`
+			<option value="0" selected disabled>Wybierz walutę</option>
+			<option name="PLN" value="1.0">PLN&nbsp-&nbspzłoty</option>
+    `);
+    $(".MoneyList").append(`
+        <tr class="Legend">
+            <th>Nazwa</th><th>Kod</th><th>Wartość</th>
+        </tr>
+    `)
+
+    $.each(data[0].rates, function (i, item) {
+
+        var mid = item.mid;
+        var code = item.code;
+        var name = item.currency;
+
+        $('.ValueSelect').append(`
+            <option name="${code}" value="${mid}">${code}&nbsp-&nbsp${name}</option>
+        `);
+        $(".MoneyList").append(`
+            <tr class="SingleValue">
+                <td>${name}</td><td>${code}</td><td>${mid}</td>
+            </tr>
+        `)
+    });
+    
+    var date = data[0].effectiveDate;
+    var mark = data[0].no;
+    
+    $(".HeaderText_Date").html(date);
+    $(".HeaderText_Mark").html(mark);
+    
+    ShowConverter("ready");
 }
 
 function TableError() {
     var errorMessage = `<span class="TableError--Text">Cos poszło nie tak. Proszę spróbować jeszcze raz.</span>`;
     $(".TableError").HTML(errorMessage);
     $(".TableError").removeClass("HiddenElement");
+}
+
+function ShowConverter(state) {
+    if (state === "ready") {
+        $(".GeneralContainer").removeClass("HiddenElement");
+        $(".StartUp").addClass("HiddenElement");
+        var convert_button = document.getElementsByClassName("ConvertButton")[0];
+        convert_button.addEventListener("click", ConvertMoney);
+        SetLoader(false);
+    } else {
+        $(".GeneralContainer").addClass("HiddenElement");
+        $(".StartUp").removeClass("HiddenElement");
+    }
+}
+
+function ConvertMoney () {
+    var first = document.getElementsByClassName("ValueSelect")[0];
+    var i = first.selectedIndex;
+    var first_currency = first.value;
+    
+    var second = document.getElementsByClassName("ValueSelect")[1];
+    var z = second.selectedIndex;
+    var second_currency = second.value;
+
+    var amount = document.getElementsByClassName("ValueInput")[0].value;
+    
+    var firstcode = first.options[i].getAttribute('name');
+    var secondcode = second.options[z].getAttribute('name');
+
+    var calculation = amount*first_currency;
+    var result = calculation/second_currency;
+
+    if (isNaN(result)) {
+        ConvertMoney_Error()
+    } else if (result === Infinity) {
+        ConvertMoney_Error()
+    } else if (result === 0) {
+        ConvertMoney_Error()
+    } else {
+        ConvertMoney_Success(firstcode, secondcode, amount, result.toFixed(2))
+    }
+}
+
+function ConvertMoney_Error() {
+    $(".ConvertScore").html(`<h2>*Uzupełnij pola</h2>`);
+}
+
+function ConvertMoney_Success(firstcode, secondcode, amount, result) {
+    $(".ConvertScore").html(`<h2>${amount} ${firstcode} = ${result} ${secondcode}</h2>`);
 }
 
 function SetLoader(status) {
@@ -47,4 +131,16 @@ function SetLoader(status) {
             $(".Loading").removeClass("Loader-Hidding");
         });
     }
+}
+
+window.addEventListener("load", placeholder);
+
+function placeholder() {
+    var money_value = document.getElementsByClassName("ValueInput")[0];
+    money_value.addEventListener("blur", function () {
+        this.placeholder = 'Wartość';
+    });
+    money_value.addEventListener("focus", function () {
+        this.placeholder = '';
+    })
 }
